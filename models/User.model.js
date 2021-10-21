2c960b207efbca9c467481668435300d4d59d615
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-10-21 12:28:22 +0200

    update user model

--- models/User.model.js
+++ models/User.model.js
@@ -1,20 +1,29 @@
-const { Schema, model } = require("mongoose");
+// models/User.model.js
+const { Schema, model } = require('mongoose');
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
     username: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      trim: true,
+      required: [true, 'Username is required.'],
+      unique: true
     },
-    password: String,
+    email: {
+      type: String,
+      required: [true, 'Email is required.'],
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    passwordHash: {
+      type: String,
+      required: [true, 'Password is required.']
+    }
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
+    timestamps: true
   }
 );
 
-const User = model("User", userSchema);
-
-module.exports = User;
+module.exports = model('User', userSchema);
