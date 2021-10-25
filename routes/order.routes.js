d7d610d43bd8d1db0e13cd68af3e25c4f488e13f
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2021-10-25 09:46:36 +0200

    minor: commint on a different file

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const isAdmin = require("../middelware/isAdmin.middelware");
 
 router.get("/orders", isAdmin, (req, res, next) => {
+    console.log("he karina, I'm working on the order routes....")
     res.send("display list of orders (includes billing and sensitive information) ")
 });
 
